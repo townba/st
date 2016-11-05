7536fec73f9137a254dc48fa8d151a00925c2d3f
Brad Town <brad@bradtown.com>
2016-11-04 18:16:13 -0700

    Fix various combinations of faint, bold, and reverse text.

--- st.c
+++ st.c
@@ -441,6 +441,7 @@ static void tstrsequence(uchar);
 
 static inline ushort sixd_to_16bit(int);
 static int xmakeglyphfontspecs(XftGlyphFontSpec *, const Glyph *, int, int, int);
+static Color *xgetcolor(uint32_t, XRenderColor *, Color *);
 static void xdrawglyphfontspecs(const XftGlyphFontSpec *, Glyph, int, int, int);
 static void xdrawglyph(Glyph, int, int);
 static void xhints(void);
@@ -4039,15 +4040,30 @@ xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len, int x
 	return numspecs;
 }
 
+Color *
+xgetcolor(uint32_t basecol, XRenderColor *rendercol, Color *truecol)
+{
+	if (!IS_TRUECOL(basecol))
+		return &dc.col[basecol];
+	rendercol->alpha = 0xffff;
+	rendercol->red = TRUERED(basecol);
+	rendercol->green = TRUEGREEN(basecol);
+	rendercol->blue = TRUEBLUE(basecol);
+	XftColorAllocValue(xw.dpy, xw.vis, xw.cmap, rendercol, truecol);
+	return truecol;
+}
+
 void
 xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, int y)
 {
 	int charlen = len * ((base.mode & ATTR_WIDE) ? 2 : 1);
 	int winx = borderpx + x * xw.cw, winy = borderpx + y * xw.ch,
 	    width = charlen * xw.cw;
-	Color *fg, *bg, *temp, revfg, revbg, truefg, truebg;
+	Color *fg, *bg, revfg, revbg, truefg, truebg;
+	Color **destfg, **destbg;
 	XRenderColor colfg, colbg;
 	XRectangle r;
+	uint32_t srcfg;
 
 	/* Fallback on color display for attributes not supported by the font */
 	if (base.mode & ATTR_ITALIC && base.mode & ATTR_BOLD) {
@@ -4058,31 +4074,20 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 		base.fg = defaultattr;
 	}
 
-	if (IS_TRUECOL(base.fg)) {
-		colfg.alpha = 0xffff;
-		colfg.red = TRUERED(base.fg);
-		colfg.green = TRUEGREEN(base.fg);
-		colfg.blue = TRUEBLUE(base.fg);
-		XftColorAllocValue(xw.dpy, xw.vis, xw.cmap, &colfg, &truefg);
-		fg = &truefg;
-	} else {
-		fg = &dc.col[base.fg];
-	}
+	fg = xgetcolor(base.fg, &colfg, &truefg);
+	bg = xgetcolor(base.bg, &colbg, &truebg);
 
-	if (IS_TRUECOL(base.bg)) {
-		colbg.alpha = 0xffff;
-		colbg.green = TRUEGREEN(base.bg);
-		colbg.red = TRUERED(base.bg);
-		colbg.blue = TRUEBLUE(base.bg);
-		XftColorAllocValue(xw.dpy, xw.vis, xw.cmap, &colbg, &truebg);
-		bg = &truebg;
-	} else {
-		bg = &dc.col[base.bg];
-	}
+	/*
+	 * When reversing, we change the background instead of the foreground
+	 * because we swap them later.
+	 */
+	destfg = !(base.mode & ATTR_REVERSE) ? &fg : &bg;
+	destbg = !(base.mode & ATTR_REVERSE) ? &bg : &fg;
+	srcfg = !(base.mode & ATTR_REVERSE) ? base.fg : base.bg;
 
 	/* Change basic system colors [0-7] to bright system colors [8-15] */
-	if ((base.mode & ATTR_BOLD_FAINT) == ATTR_BOLD && BETWEEN(base.fg, 0, 7))
-		fg = &dc.col[base.fg + 8];
+	if ((base.mode & ATTR_BOLD_FAINT) == ATTR_BOLD && BETWEEN(srcfg, 0, 7))
+		*destfg = &dc.col[srcfg + 8];
 
 	if (IS_SET(MODE_REVERSE)) {
 		if (fg == &dc.col[defaultfg]) {
@@ -4110,25 +4115,20 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 		}
 	}
 
-	if (base.mode & ATTR_REVERSE) {
-		temp = fg;
-		fg = bg;
-		bg = temp;
-	}
-
 	if ((base.mode & ATTR_BOLD_FAINT) == ATTR_FAINT) {
-		colfg.red = fg->color.red / 2;
-		colfg.green = fg->color.green / 2;
-		colfg.blue = fg->color.blue / 2;
+		colfg.red = (*destfg)->color.red / 2;
+		colfg.green = (*destfg)->color.green / 2;
+		colfg.blue = (*destfg)->color.blue / 2;
+		colfg.alpha = (*destfg)->color.alpha;
 		XftColorAllocValue(xw.dpy, xw.vis, xw.cmap, &colfg, &revfg);
-		fg = &revfg;
+		*destfg = &revfg;
 	}
 
 	if (base.mode & ATTR_BLINK && term.mode & MODE_BLINK)
-		fg = bg;
+		destfg = destbg;
 
 	if (base.mode & ATTR_INVISIBLE)
-		fg = bg;
+		destfg = destbg;
 
 	/* Intelligent cleaning up of the borders. */
 	if (x == 0) {
@@ -4145,7 +4145,7 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 		xclear(winx, winy + xw.ch, winx + width, xw.h);
 
 	/* Clean up the region we want to draw to. */
-	XftDrawRect(xw.draw, bg, winx, winy, width, xw.ch);
+	XftDrawRect(xw.draw, *destbg, winx, winy, width, xw.ch);
 
 	/* Set the clip region because Xft is sometimes dirty. */
 	r.x = 0;
@@ -4155,17 +4155,17 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 	XftDrawSetClipRectangles(xw.draw, winx, winy, &r, 1);
 
 	/* Render the glyphs. */
-	XftDrawGlyphFontSpec(xw.draw, fg, specs, len);
+	XftDrawGlyphFontSpec(xw.draw, *destfg, specs, len);
 
 	/* Render underline and strikethrough. */
 	if (base.mode & ATTR_UNDERLINE) {
-		XftDrawRect(xw.draw, fg, winx, winy + dc.font.ascent + 1,
-				width, 1);
+		XftDrawRect(xw.draw, *destfg,
+				winx, winy + dc.font.ascent + 1, width, 1);
 	}
 
 	if (base.mode & ATTR_STRUCK) {
-		XftDrawRect(xw.draw, fg, winx, winy + 2 * dc.font.ascent / 3,
-				width, 1);
+		XftDrawRect(xw.draw, *destfg,
+				winx, winy + 2 * dc.font.ascent / 3, width, 1);
 	}
 
 	/* Reset clip to none. */
